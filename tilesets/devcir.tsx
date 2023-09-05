65daf0d357485411ad821667823b0df5167344bd
Frank Flimm <65899958+frankflimm@users.noreply.github.com>
2023-09-05 13:04:36 +0200

    Embed Tileset 3

--- tilesets/devcir.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="git" name="devcir" tilewidth="32" tileheight="32" tilecount="4" columns="2">
- <image source="../../../../PROJEKTE/WorkAdventure/tiles/devcir_logo.png" width="64" height="64"/>
-</tileset>
