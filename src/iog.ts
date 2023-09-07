e80ae8f75f3250a4a93775139d56f7515a4fc7d8
Frank Flimm <65899958+frankflimm@users.noreply.github.com>
2023-09-07 21:52:39 +0200

    Silent zones

---
+++ src/iog.ts
@@ -0,0 +1,60 @@
+/// <reference types="@workadventure/iframe-api-typings" />
+
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+
+console.log('Script started successfully');
+
+let currentPopup: any = undefined;
+
+// Waiting for the API to be ready
+WA.onInit().then(() => {
+    console.log('Scripting API ready');
+    console.log('Player tags: ',WA.player.tags)
+
+    // Clock
+    WA.room.area.onEnter('clock').subscribe(() => {
+        const today = new Date();
+        const time = today.getHours() + ":" + today.getMinutes();
+        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    })
+    WA.room.area.onLeave('clock').subscribe(closePopup)
+
+//     // Wiki
+//     WA.room.area.onEnter('wiki').subscribe(() => {
+//         WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+//     })
+//     WA.room.area.onLeave('wiki').subscribe()
+
+    // Boss
+    var halleluja = WA.sound.loadSound("halleluja.mp3");
+    var config = {
+        volume : 1.0,
+        loop : false,
+        rate : 1,
+        detune : 1,
+        delay : 0,
+        seek : 0,
+        mute : false
+    }
+    WA.room.area.onEnter('boss').subscribe(() => {
+        halleluja.play(config);
+    })
+    WA.room.area.onLeave('boss').subscribe(() => {
+        halleluja.stop();
+    })
+
+    // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
+    bootstrapExtra().then(() => {
+        console.log('Scripting API Extra ready');
+    }).catch(e => console.error(e));
+
+}).catch(e => console.error(e));
+
+function closePopup(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+
+export {};
